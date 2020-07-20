51c12e1ed2c9960e3840de0b3e1c067c3f353423
Michael Bestas <mkbestas@lineageos.org>
2020-07-20 21:32:44 +0300

    grus: Remove android.hardware.contexthub
    Change-Id: I62bc93aee19c8070e4563825a07cfc48774e9b93

--- device.mk
+++ device.mk
@@ -181,11 +181,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
-# Context Hub
-PRODUCT_PACKAGES += \
-    android.hardware.contexthub@1.0-impl.generic \
-    android.hardware.contexthub@1.0-service
-
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
