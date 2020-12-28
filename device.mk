828585195b3fb7e712f44b3bdc86df4f6a83e528
uixdess <alexeydomoratsky1@gmail.com>
2020-12-28 12:05:24 +0300

    grus: Add security config for IRSC

--- device.mk
+++ device.mk
@@ -272,6 +272,10 @@ PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml
 
+# IRSC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_grus
