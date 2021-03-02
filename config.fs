67f71e9e58855c34263557ae86134fe8d23a82b8
Ivan Vecera <ivan@cera.cz>
2021-03-02 13:02:07 +0100

    grus: config.fs: Remove non-existing entries
    We do not ship slim_daemon nor xtwifi-client... Remove corresponding
    entris from config.fs.
    
    Signed-off-by: Ivan Vecera <ivan@cera.cz>
    Change-Id: I632e4a795fe9a70328b1ac620eaff5a918f0c7cc

--- config.fs
+++ config.fs
@@ -61,47 +61,17 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
 
-[vendor/bin/slim_daemon]
-mode: 0755
-user:  AID_GPS
-group: AID_GPS
-caps: NET_BIND_SERVICE
-
 [vendor/bin/loc_launcher]
 mode: 0755
 user:  AID_GPS
 group: AID_GPS
 caps: SETUID SETGID
 
-[vendor/bin/xtwifi-client]
-mode: 0755
-user:  AID_GPS
-group: AID_GPS
-caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
-
 [vendor/bin/sensors.qti]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
-
-[vendor/bin/glgps]
-mode: 0755
-user: AID_GPS
-group: AID_SYSTEM
-caps: BLOCK_SUSPEND
-
-[vendor/bin/ignss_2_0]
-mode: 0755
-user: AID_GPS
-group: AID_SYSTEM
-caps: BLOCK_SUSPEND
-
-[vendor/bin/lhd]
-mode: 0755
-user: AID_GPS
-group: AID_SYSTEM
-caps: BLOCK_SUSPEND
 
 [firmware/]
 mode: 0771
