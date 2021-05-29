8f7c3dde57c8dde02626c52ff5651f5fd230dc2a
Andrey Sprynga <rtx4d@aospa.co>
2021-05-29 18:29:44 +0700

    grus: Adapt FOD for Android 11
    Signed-off-by: Andrey Sprynga <rtx4d@aospa.co>
    Change-Id: I43151e357903905d69765fb1a77ffd4cab3b0c65

--- device.mk
+++ device.mk
@@ -220,6 +220,8 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_grus \
+    libkeymaster_messages.vendor \
+    libkeymaster_portable.vendor \
     vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_grus
 
 # GPS
