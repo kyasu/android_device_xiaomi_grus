822cce493a9df4f3c7a3d7909e39dfaf1c5a719b
kyasu <kyasu.dev@gmail.com>
2021-08-10 16:38:48 +0900

    grus: Drop PocketMode app
    * The elliptic proximity sensor does not work well with PocketMode.

--- device.mk
+++ device.mk
@@ -363,10 +363,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
-# PocketMode
-PRODUCT_PACKAGES += \
-    XiaomiPocketMode
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
