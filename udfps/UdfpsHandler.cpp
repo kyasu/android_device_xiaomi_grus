a09ae82ec143e76b4c326e9ecaeef4273c41b3f8
Bruno Martins <bgcngm@gmail.com>
2022-09-09 14:41:42 +0100

    grus: udfps: Update handler with newly introduced methods
    Just stubs, at least for now.
    
    Change-Id: Iff5604b35368ad5f417265df4e2eaf5a09ff7d74

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -78,6 +78,15 @@ class XiaomiGrusUdfpsHander : public UdfpsHandler {
     void onFingerUp() {
         // nothing
     }
+
+    void onAcquired(int32_t /*result*/, int32_t /*vendorCode*/) {
+        // nothing
+    }
+
+    void cancel() {
+        // nothing
+    }
+
   private:
     fingerprint_device_t *mDevice;
 };
