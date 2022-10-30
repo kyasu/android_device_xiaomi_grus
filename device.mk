7b0dabcfad3b4c1265193d73557c9028f6fda5b7
kyasu <kyasu.dev@gmail.com>
2022-10-30 16:40:17 +0900

    grus: Use bluetooth blobs from grus V12.0.4.0.QFBMIXM global

--- device.mk
+++ device.mk
@@ -143,8 +143,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
+    vendor.qti.hardware.btconfigstore@1.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
