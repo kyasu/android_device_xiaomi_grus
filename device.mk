d45cf86aca7d13960e6ed955ae6907211d03bd77
Alexander Martinz <amartinz@shiftphones.com>
2022-11-11 16:06:29 +0100

    grus: audio: switch to open source sound trigger hal
    The audio hal expects the version of the android and sound trigger
    hal to match.
    
    As the prebuilt sound trigger hal is built with a BSP based on
    Android 10, the version is older than expected and sound trigger fails
    to initialize.
    
    Test: atest CtsSoundTriggerTestCases
    Test: atest VtsHalSoundtriggerV2_0TargetTest
    Test: atest VtsHalSoundtriggerV2_1TargetTest
    Test: atest VtsHalSoundtriggerV2_2TargetTest
    
    Depends-On: https://review.lineageos.org/344863
    Change-Id: I23b052149bf243f86ebfa13dbbc5053abe4b92e1
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- device.mk
+++ device.mk
@@ -108,7 +108,8 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing \
     libtinycompress \
     libvolumelistener \
-    tinymix
+    tinymix \
+    sound_trigger.primary.sdm710
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
