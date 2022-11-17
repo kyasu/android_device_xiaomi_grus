4df2008ca3b954efddc54949caa612c3ce4b00fa
Bruno Martins <bgcngm@gmail.com>
2022-11-17 14:24:56 +0000

    grus: Drop updatable_apex inclusion and enable compression
    The inclusion of the makefile has been moved back to the
    common device tree and compression can be enabled because
    kernel now supports it.
    
    Change-Id: I97383c7c63e3946f4c3c0af4be2f9b63ec304b7e

--- device.mk
+++ device.mk
@@ -6,11 +6,6 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
-PRODUCT_COMPRESSED_APEX := false
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
