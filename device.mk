6231f3b7305bf012b1701b8b0bd18b26b8fa7f25
kyasu <kyasu.dev@gmail.com>
2023-01-11 14:28:09 +0900

    Revert "grus: Use bluetooth blobs from grus V12.0.4.0.QFBMIXM global"
    This reverts commit 7b0dabcfad3b4c1265193d73557c9028f6fda5b7.

--- device.mk
+++ device.mk
@@ -134,11 +134,12 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth@1.1.vendor \
     android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
