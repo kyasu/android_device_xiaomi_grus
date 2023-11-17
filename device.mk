123fb275d0004f83ce15902471f36646c6d06c6f
kyasu <kyasu.dev@gmail.com>
2023-11-17 15:55:20 +0900

    grus: Build libprotobuf-cpp-{lite,full}-3.9.1-vendorcompat

--- device.mk
+++ device.mk
@@ -382,7 +382,9 @@ PRODUCT_COPY_FILES += \
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
-    libprotobuf-cpp-lite-vendorcompat
+    libprotobuf-cpp-lite-vendorcompat \
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
 
 # QTI
 PRODUCT_PACKAGES += \
