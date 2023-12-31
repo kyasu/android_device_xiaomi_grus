40cb4349a3442b29854bb4ef743ac2e8d5a55fe0
me_cafebabe <me_cafebabe@hotmail.com>
2023-12-31 23:22:51 +0700

    grus: Switch to QTI Vibrator AIDL
    It provides haptic effects with qti haptics driver,
    Meanwhile still supports simple vibration with qpnp haptics driver.
    
    Co-authored-by: Roman Rihter <teledurak@gmail.com>
    Change-Id: I83d0c5f562c855afba563e7b959d3e0cd697ef26

--- device.mk
+++ device.mk
@@ -464,8 +464,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl:64 \
-    android.hardware.vibrator@1.0-service
+    vendor.qti.hardware.vibrator.service
 
 # VNDK
 PRODUCT_COPY_FILES += \
