3c8d48b0fb884ee313aaec52090033343745211a
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-19 11:26:40 +0800

    grus: Build AOSP Radio HIDL->AIDL Wrapper
    Change-Id: Iaeb8d37f4a7f02e6e812bc4da1b413c9a6493f8f

--- device.mk
+++ device.mk
@@ -398,6 +398,7 @@ PRODUCT_COPY_FILES += \
 # Radio
 PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.2.vendor \
+    android.hardware.radio-service.compat \
     android.hardware.radio@1.5.vendor \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
