809102d390ddb1e540c776596880db4e458586a2
Yumi Yukimura <me.cafebabe@gmail.com>
2024-11-04 20:55:55 +0800

    grus: Switch to AIDL USB Gadget HAL
    Change-Id: Ic561f62588232449c3bc1e887fc68defea634ecd

--- device.mk
+++ device.mk
@@ -382,7 +382,7 @@ PRODUCT_PACKAGES += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb \
-    android.hardware.usb.gadget@1.2-service-qti
+    android.hardware.usb.gadget-service.qti
 
 PRODUCT_PACKAGES += \
     usb_compositions.conf
