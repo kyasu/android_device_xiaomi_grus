b905124bbb27db74291d568d45a08611e905511b
LuK1337 <priv.luk@gmail.com>
2024-12-05 16:41:51 +0100

    grus: Build libqti_vndfwk_detect.vendor:32
    Before:
    audio_hw_extn: spkr_prot_feature_init: Called with feature Enabled, vendor_enhanced_info 0x0
    
    After:
    audio_hw_extn: spkr_prot_feature_init: Called with feature Enabled, vendor_enhanced_info 0x3
    
    Change-Id: Ie5c33eafc1e0be2eeda8e0a6bbbc3712e32986e9

--- device.mk
+++ device.mk
@@ -395,6 +395,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/permissions/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
 
+# QTI fwk-detect
+PRODUCT_PACKAGES += \
+    libqti_vndfwk_detect.vendor:32
+
 # Radio
 PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.2.vendor \
