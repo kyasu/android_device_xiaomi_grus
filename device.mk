d57f59a35b4b8ae6f4e7dbbfdda91c6446de3c65
kyasu <kyasu.dev@gmail.com>
2025-02-11 11:50:46 +0900

    grus: Move to Xiaomi fingerprint AIDL
    Change-Id: If4d695505a78879b73fbfd6fb5dc4281f6c81964

--- device.mk
+++ device.mk
@@ -223,7 +223,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
+    android.hardware.biometrics.fingerprint-service.xiaomi \
     libkeymaster_messages.vendor \
     libkeymaster_portable.vendor \
     liblzma.vendor \
