c0a24581bbc307a05d41d44694ee5fb6398dc581
kyasu <kyasu.dev@gmail.com>
2025-03-14 08:24:39 +0900

    grus: Remove copy libcutils-v29
    Change-Id: I511e3bc2acef535858f669b1137076a2ecd70b04

--- device.mk
+++ device.mk
@@ -466,10 +466,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
-# VNDK
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libcutils-v29.so
-
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
