43a187b8f60ea801bbc45967035018cdbc43f8d2
Yumi Yukimura <me.cafebabe@gmail.com>
2025-03-19 23:28:20 +0800

    grus: Disable VINTF kernel requirements
    Change-Id: I12a75936976078a29fa0d15e24ced97d1ad9e4c1

--- device.mk
+++ device.mk
@@ -298,6 +298,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
 # Kernel
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
 PRODUCT_ENABLE_UFFD_GC := false
 
 # Keymaster
