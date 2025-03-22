b139474bd62c7173a0e4db233b78abb987c3ad32
kyasu <kyasu.dev@gmail.com>
2025-03-22 10:21:32 +0900

    grus: Disable UFFD GC
    Change-Id: Icc8b5c008558b824a93b46f4ccb3311bbce653a7

--- device.mk
+++ device.mk
@@ -297,6 +297,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
+# Kernel
+PRODUCT_ENABLE_UFFD_GC := false
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
