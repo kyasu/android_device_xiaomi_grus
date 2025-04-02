0528b3ed0474285cebc9a3aa2a12d8b67a2a4a79
Nolen Johnson <johnsonnolen@gmail.com>
2025-04-02 18:24:56 -0400

    grus: Kill all BUILD_BROKEN_* flags
    Change-Id: I11f39635412d1d6b74e6ca05c48e02ced70b43a7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,11 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-BUILD_BROKEN_DUP_RULES := true
-
 BOARD_VENDOR := xiaomi
-
-BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 DEVICE_PATH := device/xiaomi/grus
 
