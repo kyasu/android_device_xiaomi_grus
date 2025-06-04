e5f3bb9cd607905080589a3c4ca51c8ee606c274
kyasu <kyasu.dev@gmail.com>
2025-06-04 15:47:04 +0900

    grus: Switch to common AIDL IR service
    Change-Id: I1918bd92373805dab6439c5f281e23ab97bfbbdc

--- device.mk
+++ device.mk
@@ -164,7 +164,7 @@ PRODUCT_PACKAGES += \
 
 # ConsumerIR
 PRODUCT_PACKAGES += \
-    android.hardware.ir-service.xiaomi
+    android.hardware.ir-service.lineage
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
