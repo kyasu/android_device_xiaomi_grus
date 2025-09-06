1c7acfb7becc9fe7ccd12276407999b66805705f
Jaegeuk Kim <jaegeuk@google.com>
2025-09-06 17:43:16 +0200

    grus: use metadata_csum for /metadata
    This enhances ext4 stability.
    
    Bug: 149391799
    Signed-off-by: Jaegeuk Kim <jaegeuk@google.com>
    Change-Id: I8cebe30bdf66f39f0e4cc5955e53aca4d4f22f64

--- device.mk
+++ device.mk
@@ -196,6 +196,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
 
+# Filesystem
+PRODUCT_PACKAGES += \
+    e2fsck_ramdisk \
+    tune2fs_ramdisk \
+    resize2fs_ramdisk
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint-service.xiaomi \
