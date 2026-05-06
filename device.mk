08f6a85d2054c3fcea11bb422dee7d1ceff724c2
kyasu <kyasu.dev@gmail.com>
2026-05-06 15:25:03 +0900

    grus: lineage health: Switch to input_suspend
    Change-Id: I3907440b6788627c50f94f12018a3885ba7a7ba2

--- device.mk
+++ device.mk
@@ -261,6 +261,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
+$(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay-service.sdm
